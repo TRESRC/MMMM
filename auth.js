53208c60453746d1e3b36a0cd2c3689b9b453a45
TRESRC <rc@theresource.tv>
2026-05-13 16:49:51 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,9 @@
 // api/auth.js
 // Validates username:password against the stored hash server-side
 
-import { createHash } from "crypto";
+const { createHash } = require("crypto");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
